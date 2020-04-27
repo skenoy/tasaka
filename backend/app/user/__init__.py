7f05cdead6da6e529aa61f069cbc755d7bc5fabe
skenoy <skenoy@gmail.com>
2020-04-27 14:47:12 +0800

    fix python format

--- backend/app/user/__init__.py
+++ backend/app/user/__init__.py
@@ -9,39 +9,39 @@ user = Blueprint('user', __name__)
 
 @user.route("/validatecode", methods=['POST'])
 def validatecode():
-	data = request.json['data']
-	email = User.query.filter(User.email==data['email']).first()
-	if email:
-		return jsonify({'msg': 'This email is exists!', 'code': 410})
-	else:
-		code = ''
-		while 1:
-			code = User.query.filter(User.validatecode==uuid4().hex).first()
-			if not code:
-				break
-		try:
-			vc_user = User()
-			vc_user.email = data['email']
-			vc_user.validatecode = code
-			db.session.add(vc_user)
-			db.session.commit()
-			email('Tasaka邮箱验证', data['email'], 'sendcode', validatecode=code)
-		except:
-			db.session.rollback()
-			return jsonify({'msg': 'The database is error!', 'code': 420})
-		return jsonify({'msg': 'Send email validate code！', 'code': 200})
+    data = request.json['data']
+    email = User.query.filter(User.email==data['email']).first()
+    if email:
+        return jsonify({'msg': 'This email is exists!', 'code': 410})
+    else:
+        code = ''
+        while 1:
+            code = User.query.filter(User.validatecode==uuid4().hex).first()
+            if not code:
+                break
+        try:
+            vc_user = User()
+            vc_user.email = data['email']
+            vc_user.validatecode = code
+            db.session.add(vc_user)
+            db.session.commit()
+            email('Tasaka邮箱验证', data['email'], 'sendcode', validatecode=code)
+        except:
+            db.session.rollback()
+            return jsonify({'msg': 'The database is error!', 'code': 420})
+        return jsonify({'msg': 'Send email validate code！', 'code': 200})
 
 @user.route("/register", methods=['POST'])
 def register():
     data = request.json['data']
-	
+    
     code = User.query.filter(User.validatecode == data['validatecode']).first()
     if not code:
         return jsonify({'msg': 'Email validate code is error!', 'code': 440})
     email = User.query.filter(User.email == data['email']).first()
     if not code:
         return jsonify({'msg': 'Email is error!', 'code': 450})
-	name = User.query.filter(User.name==data['name']).first()
+    name = User.query.filter(User.name==data['name']).first()
     if name:
         return jsonify({'msg': 'name is exists!', 'code': 430})
     try:
@@ -53,8 +53,8 @@ def register():
         db.session.add(r_user)
         db.session.commit()
     except:
-		db.session.rollback()
-		return jsonify({'msg': 'Add user fail！', 'code': 460})
+        db.session.rollback()
+        return jsonify({'msg': 'Add user fail！', 'code': 460})
     return jsonify({'msg': 'Add user success！', 'code': 200})
 
 @user.route("/login", methods=['POST'])
